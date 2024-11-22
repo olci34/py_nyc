8c5657fe84a6ed0c1066fe69472be16517cfd4f4
Murat Ogulcan Sahin <developerogulcan@gmail.com>
2024-11-22 02:09:37 -0500

    Cors config and Pipenv script (#2)
    * Display NYC taxi zones in the map
    
    * Add cors and pipenv script

--- py_nyc/web/server.py
+++ py_nyc/web/server.py
@@ -3,10 +3,22 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from py_nyc.web.api.api import router as trips_router
-
+from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
 server = FastAPI(debug=True)
+
+server.add_middleware(CORSMiddleware,
+                      allow_origins=origins,
+                      allow_credentials=True,
+                      allow_methods=["*"],
+                      allow_headers=["*"])
 
 server.mount(
     "/static", StaticFiles(directory="./py_nyc/web/static"), name="static")
